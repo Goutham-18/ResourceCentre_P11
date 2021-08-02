7dc06fc0eb31848353abe3816bba410a104c8bc2
Goutham <20026610@myrp.edu.sg>
2021-08-02 13:45:09 +0800

    OPTION_QUIT

--- ResourceCentre_P11/src/ResourceCentre.java
+++ ResourceCentre_P11/src/ResourceCentre.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 public class ResourceCentre {
 
 
+	private static final int OPTION_QUIT = 5;
 	public static void main(String[] args) {
 
 		ArrayList<Camcorder> camcorderList = new ArrayList<Camcorder>();
@@ -15,7 +16,7 @@ public class ResourceCentre {
 
 		int option = 0;
 
-		while (option != 5) {
+		while (option != OPTION_QUIT) {
 
 			ResourceCentre.menu();
 			option = Helper.readInt("Enter an option > ");
@@ -129,7 +130,8 @@ public class ResourceCentre {
 
 		for (int i = 0; i < camcorderList.size(); i++) {
 
-			output += String.format("%-10s %-30s %-10s %-10s %-20d\n", camcorderList.get(i).getAssetTag(),
+			String assetTag = camcorderList.get(i).getAssetTag();
+			output += String.format("%-10s %-30s %-10s %-10s %-20d\n", assetTag,
 					camcorderList.get(i).getDescription(), 
 					ResourceCentre.showAvailability(camcorderList.get(i).getIsAvailable()),
 					camcorderList.get(i).getDueDate(),camcorderList.get(i).getOpticalZoom());
